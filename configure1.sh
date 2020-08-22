5e5a1d13de3ca32ef72cd32bab67fe693321ad63
jaskon139 <jaskon139@gmail.com>
2020-08-22 17:02:45 +0800

    Create configure1.sh

---
+++ configure1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -L -H "Cache-Control: no-cache" -o /tmp/configure.sh https://github.com/jaskon139/v2ray-heroku-1/raw/master/configure.sh
+
+chmod +x /tmp/configure.sh
+
+bash /tmp/configure.sh
